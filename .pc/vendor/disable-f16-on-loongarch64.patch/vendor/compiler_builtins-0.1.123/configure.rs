28dcb37ebc98687d84aea36422f4bc431cbaef9b
LFRon <ronforever@outlook.com>
2026-05-21 14:18:58 +0800

    fix: 关闭FP16构建
    deepin v25的龙芯LLVM-19使用FP16编译会出错,故禁用

--- .pc/vendor/disable-f16-on-loongarch64.patch/vendor/compiler_builtins-0.1.123/configure.rs
+++ .pc/vendor/disable-f16-on-loongarch64.patch/vendor/compiler_builtins-0.1.123/configure.rs
@@ -70,8 +70,6 @@ pub fn configure_f16_f128(target: &Target) {
         "powerpc64" if &target.os == "aix" => (true, false),
         // `f128` crashes <https://github.com/llvm/llvm-project/issues/41838>
         "sparc" | "sparcv9" => (true, false),
-        // `f16` crashes during instruction selection for the soft-float builtins.
-        "loongarch64" => (false, true),
         // `f16` miscompiles <https://github.com/llvm/llvm-project/issues/96438>
         "wasm32" | "wasm64" => (false, true),
         // Most everything else works as of LLVM 19
